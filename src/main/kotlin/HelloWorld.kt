198c0177b1f9d239f3e04d59fea5dfbe0e120cb7
bressani <jr.bressani@hotmail.com>
2019-06-05 18:06:18 -0300

    Improve hello world routing

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,21 +1,32 @@
+import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
-import io.ktor.http.ContentType
-import io.ktor.response.respondText
-import io.ktor.routing.Routing
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
 import io.ktor.routing.get
+import io.ktor.routing.route
+import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun Application.module() {
     install(DefaultHeaders)
     install(CallLogging)
-    install(Routing) {
-        get("/") {
-            call.respondText("Hello World!", ContentType.Text.Html)
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    routing {
+        route("/") {
+            get {
+                call.respond(mapOf("Hello" to "World!"))
+            }
         }
     }
 }
